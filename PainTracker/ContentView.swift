1a6854068438b7121ae9c8ba670c6414941ce367
Kristoffer Anger <kristoffer.anger@gmail.com>
2019-09-04 14:01:29 +0200

    Initial Commit

---
+++ PainTracker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PainTracker
+//
+//  Created by Kristoffer Anger on 2019-09-04.
+//  Copyright © 2019 Kriang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
