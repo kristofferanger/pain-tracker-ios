ecb620b1406189a576aebb00ec3510cbf81feb5f
Kristoffer Anger <kristoffer.anger@gmail.com>
2019-09-06 09:22:14 +0200

    First commit

---
+++ PainTracker/ColorHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  ColorHelpers.swift
+//  PainTracker
+//
+//  Created by Kristoffer Anger on 2019-09-04.
+//  Copyright © 2019 Kriang. All rights reserved.
+//
+
+import Foundation
---
+++ PainTracker/MeterView.swift
@@ -0,0 +1,23 @@
+//
+//  MeterView.swift
+//  PainTracker
+//
+//  Created by Kristoffer Anger on 2019-09-05.
+//  Copyright © 2019 Kriang. All rights reserved.
+//
+
+import SwiftUI
+
+struct MeterView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MeterView_Previews : PreviewProvider {
+    static var previews: some View {
+        MeterView()
+    }
+}
+#endif
